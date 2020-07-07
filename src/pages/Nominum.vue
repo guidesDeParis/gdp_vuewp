8f5f89e610d0e1667efb6fc9bd938f4336da28b6
Bachir Soussi Chiadmi <bachir@figureslibres.io>
2020-07-07 15:33:43 +0200

    created index nominum occurences display and links

--- src/pages/Nominum.vue
+++ src/pages/Nominum.vue
@@ -19,13 +19,32 @@
           v-for="ed in content.occurences"
           :key="ed.item"
         >
-          <h3>{{ ed.item }}</h3>
-          <ul>
+          <h3>
+            <a
+              :href="'/edition/'+ed.item"
+              :uuid="ed.item"
+              @click.prevent="onclick"
+              @keyup.enter="onclick"
+            >
+              {{ ed.item }}
+            </a>
+          </h3>
+          <ul >
             <li
               v-for="oc in ed.occurences"
               :key="oc.uuid"
             >
-              <h4>{{ oc.title }}</h4>
+              <h4>
+                <a
+                  :href="'/edition/'+ed.item+'/'+oc.uuid"
+                  :uuid="oc.uuid"
+                  :eduuid="ed.item"
+                  @click.prevent="onclick"
+                  @keyup.enter="onclick"
+                >
+                  {{ oc.title }}
+                </a>
+              </h4>
             </li>
           </ul>
         </li>
@@ -62,6 +81,27 @@ export default {
         console.warn('Issue with nominum', error)
         Promise.reject(error)
       })
+  },
+  methods: {
+    onclick (e) {
+      console.log('clicked on nominum text occurence', e)
+      if (e.target.getAttribute('eduuid')) {
+        this.$router.push({
+          name: `editiontext`,
+          params: {
+            id: e.target.getAttribute('eduuid'),
+            textid: e.target.getAttribute('uuid')
+          }
+        })
+      } else {
+        this.$router.push({
+          name: `edition`,
+          params: {
+            id: e.target.getAttribute('uuid')
+          }
+        })
+      }
+    }
   }
 }
 </script>
