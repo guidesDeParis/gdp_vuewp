2c7b8004f5aa0c09bada541d6643a061f7e558df
Bachir Soussi Chiadmi <bachir@figureslibres.io>
2020-07-21 17:49:58 +0200

    disabled index occurence loading

--- src/components/Content/Occurence.vue
+++ src/components/Content/Occurence.vue
@@ -11,7 +11,7 @@
         {{ oc.title }}
       </a>
     </h4>
-    <span
+    <!-- <span
       class="open-close"
       @click.prevent="onOpenPreview"
       @keyup.enter="onOpenPreview"
@@ -19,9 +19,9 @@
       <svg xmlns="http://www.w3.org/2000/svg" width="14" height="10" role="presentation" class="vs__open-indicator">
         <path d="M9.211364 7.59931l4.48338-4.867229c.407008-.441854.407008-1.158247 0-1.60046l-.73712-.80023c-.407008-.441854-1.066904-.441854-1.474243 0L7 5.198617 2.51662.33139c-.407008-.441853-1.066904-.441853-1.474243 0l-.737121.80023c-.407008.441854-.407008 1.158248 0 1.600461l4.48338 4.867228L7 10l2.211364-2.40069z" />
       </svg>
-    </span>
-    <div v-if="content" class="text" v-html="tei" />
-    <a
+    </span> -->
+    <!-- <div v-if="content" class="text" v-html="tei" /> -->
+    <!-- <a
       v-if="content"
       class="lire-plus"
       :href="'/edition/'+ed.item+'/'+oc.uuid"
@@ -31,12 +31,12 @@
       @keyup.enter="onGoToText"
     >
       lire plus
-    </a>
+    </a> -->
   </section>
 </template>
 <script>
 
-import { REST } from 'api/rest-axios'
+// import { REST } from 'api/rest-axios'
 import { mapState, mapActions } from 'vuex'
 
 export default {
@@ -60,19 +60,19 @@ export default {
   },
   created () {
     console.log('occurence beforeCreate')
-    REST.get(`${window.apipath}/items/` + this.oc.uuid, {})
-      .then(({ data }) => {
-        console.log('occurence text REST: data', data)
-        if (data.content) {
-          this.content = data.content
-          const subString = this.content.tei.substr(0, 500)
-          this.tei = subString.substr(0, subString.lastIndexOf(' ')) + ' &hellip;'
-        }
-      })
-      .catch((error) => {
-        console.warn('Issue with nominum', error)
-        Promise.reject(error)
-      })
+    // REST.get(`${window.apipath}/items/` + this.oc.uuid, {})
+    //   .then(({ data }) => {
+    //     console.log('occurence text REST: data', data)
+    //     if (data.content) {
+    //       this.content = data.content
+    //       const subString = this.content.tei.substr(0, 500)
+    //       this.tei = subString.substr(0, subString.lastIndexOf(' ')) + ' &hellip;'
+    //     }
+    //   })
+    //   .catch((error) => {
+    //     console.warn('Issue with nominum', error)
+    //     Promise.reject(error)
+    //   })
   },
   methods: {
     ...mapActions({
