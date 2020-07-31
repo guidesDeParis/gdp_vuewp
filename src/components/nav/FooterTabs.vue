b708cfc80f2923db47096f5f99a07f3397bf44e4
Bachir Soussi Chiadmi <bachir@figureslibres.io>
2020-07-31 16:52:03 +0200

    typo fix #853

--- src/components/nav/FooterTabs.vue
+++ src/components/nav/FooterTabs.vue
@@ -24,7 +24,7 @@
               @click.prevent="openResults"
               @keydown.enter.prevent="openResults"
             >
-              Resultas
+              Resultats
             </span>
           </transition>
         </div>
